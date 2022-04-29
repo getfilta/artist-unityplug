02b7b3fda71abc94b0e2e29a5c9fdeb8b3fccddf
gazuntype <mrbigheaddd@gmail.com>
2022-04-29 21:22:30 +0100

    feat: add null checks.

--- Editor/HotKeys.cs
+++ Editor/HotKeys.cs
@@ -1,53 +1,65 @@
 #if UNITY_EDITOR
 using UnityEditor;
 
-public class HotKeys : Editor
-{
+public class HotKeys : Editor {
     [MenuItem("Filta Hotkeys/Pause or Resume Simulator &e")]
-        static void ToggleSimulatorPlaying() {
-            SimulatorBase simulator = FindObjectOfType<SimulatorBase>();
-            if (simulator._simulatorType == SimulatorBase.SimulatorType.Face) {
-                Simulator sim = simulator.gameObject.GetComponent<Simulator>();
-                if (sim.isPlaying) {
-                    sim.PauseSimulator();
-                } else {
-                    sim.ResumeSimulator();
-                }
-            } else if (simulator._simulatorType == SimulatorBase.SimulatorType.Body) {
-                BodySimulator sim = simulator.gameObject.GetComponent<BodySimulator>();
-                if (sim.isPlaying) {
-                    sim.PauseSimulator();
-                } else {
-                    sim.ResumeSimulator();
-                }
+    static void ToggleSimulatorPlaying() {
+        SimulatorBase simulator = FindObjectOfType<SimulatorBase>();
+        if (simulator == null) {
+            return;
+        }
+
+        if (simulator._simulatorType == SimulatorBase.SimulatorType.Face) {
+            Simulator sim = simulator.gameObject.GetComponent<Simulator>();
+            if (sim.isPlaying) {
+                sim.PauseSimulator();
+            } else {
+                sim.ResumeSimulator();
+            }
+        } else if (simulator._simulatorType == SimulatorBase.SimulatorType.Body) {
+            BodySimulator sim = simulator.gameObject.GetComponent<BodySimulator>();
+            if (sim.isPlaying) {
+                sim.PauseSimulator();
+            } else {
+                sim.ResumeSimulator();
             }
         }
-        
-        [MenuItem("Filta Hotkeys/Toggle Face Visualiser &f")]
-        static void ToggleFaceVisualiser() {
-            SimulatorBase simulator = FindObjectOfType<SimulatorBase>();
-            if (simulator._simulatorType == SimulatorBase.SimulatorType.Face) {
-                Simulator sim = simulator.gameObject.GetComponent<Simulator>();
-                sim.showFaceMeshVisualiser = !sim.showFaceMeshVisualiser;
-            }
+    }
+
+    [MenuItem("Filta Hotkeys/Toggle Face Visualiser &f")]
+    static void ToggleFaceVisualiser() {
+        SimulatorBase simulator = FindObjectOfType<SimulatorBase>();
+        if (simulator == null) {
+            return;
         }
-        
-        [MenuItem("Filta Hotkeys/Reset Simulator &r")]
-        static void ResetSimulatorPlaying() {
-            SimulatorBase simulator = FindObjectOfType<SimulatorBase>();
-            if (simulator._simulatorType == SimulatorBase.SimulatorType.Face) {
-                Simulator sim = simulator.gameObject.GetComponent<Simulator>();
-                sim.ResetSimulator();
-            }
+        if (simulator._simulatorType == SimulatorBase.SimulatorType.Face) {
+            Simulator sim = simulator.gameObject.GetComponent<Simulator>();
+            sim.showFaceMeshVisualiser = !sim.showFaceMeshVisualiser;
         }
-        
-        [MenuItem("Filta Hotkeys/Toggle Vertex Indices &v")]
-        static void ToggleVertexIndices() {
-            SimulatorBase simulator = FindObjectOfType<SimulatorBase>();
-            if (simulator._simulatorType == SimulatorBase.SimulatorType.Face) {
-                Simulator sim = simulator.gameObject.GetComponent<Simulator>();
-                sim.showVertexNumbers = !sim.showVertexNumbers;
-            }
+    }
+
+    [MenuItem("Filta Hotkeys/Reset Simulator &r")]
+    static void ResetSimulatorPlaying() {
+        SimulatorBase simulator = FindObjectOfType<SimulatorBase>();
+        if (simulator == null) {
+            return;
         }
+        if (simulator._simulatorType == SimulatorBase.SimulatorType.Face) {
+            Simulator sim = simulator.gameObject.GetComponent<Simulator>();
+            sim.ResetSimulator();
+        }
+    }
+
+    [MenuItem("Filta Hotkeys/Toggle Vertex Indices &v")]
+    static void ToggleVertexIndices() {
+        SimulatorBase simulator = FindObjectOfType<SimulatorBase>();
+        if (simulator == null) {
+            return;
+        }
+        if (simulator._simulatorType == SimulatorBase.SimulatorType.Face) {
+            Simulator sim = simulator.gameObject.GetComponent<Simulator>();
+            sim.showVertexNumbers = !sim.showVertexNumbers;
+        }
+    }
 }
 #endif
