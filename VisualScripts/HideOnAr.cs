398393aaccd91ed8e7433b15afcb9becfb48102f
gazuntype <mrbigheaddd@gmail.com>
2023-10-25 16:13:15 +0100

    feat: add hide on ar script

---
+++ VisualScripts/HideOnAr.cs
@@ -0,0 +1,37 @@
+using Filta.VisualScripting;
+using UnityEngine;
+
+[ExecuteAlways]
+public class HideOnAr : MonoBehaviour {
+    public enum Mode{Ar, NonAr}
+
+    public Mode hideMode;
+    private ArTriggerEvents _arTriggerEvents;
+
+    private void Awake() {
+        _arTriggerEvents = FindObjectOfType<ArTriggerEvents>();
+        
+    }
+
+    private void Start() {
+        if (_arTriggerEvents != null) {
+            _arTriggerEvents.onArModeSwitch += OnModeSwitch;
+        }
+    }
+
+    private void OnDestroy() {
+        if (_arTriggerEvents != null) {
+            _arTriggerEvents.onArModeSwitch -= OnModeSwitch;
+        }
+    }
+    
+
+    private void OnModeSwitch(object sender, bool isAr) {
+        if (hideMode == Mode.Ar) {
+            gameObject.SetActive(!isAr);
+        } else {
+            gameObject.SetActive(isAr);
+        }
+        
+    }
+}
